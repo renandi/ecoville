daee00a93986a1c32a44b1584fa716f34eef16f1
Renan Nandi <renannandi@gmail.com>
2025-10-01 20:47:56 -0300

    Adiciona implementação das entidades Usuario e Endereco

--- ecoville-back/src/main/java/senai/lab365/futurodev/ecoville_back/entity/Usuario.java
+++ ecoville-back/src/main/java/senai/lab365/futurodev/ecoville_back/entity/Usuario.java
@@ -1,4 +1,40 @@
 package senai.lab365.futurodev.ecoville_back.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import senai.lab365.futurodev.ecoville_back.enums.Perfil;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+
 public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false, length = 100)
+    private String nome;
+
+    @Column(nullable = false, length = 50)
+    private String nome_de_usuario;
+
+    @Column(nullable = false, length = 100)
+    private String email;
+
+    @Column(nullable = false)
+    private String senha;
+
+    @Column(nullable = false)
+    private Perfil perfil = Perfil.RESIDENCIAL;
+
+    @OneToOne
+    @JoinColumn(name="endereco_id", nullable = false)
+    private Endereco id_endereco;
+
 }
