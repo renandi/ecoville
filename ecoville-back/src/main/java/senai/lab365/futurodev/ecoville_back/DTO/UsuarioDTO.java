364cc0dd850c2aa8a17569976403bc7fab65c1e8
josemasbox <josemalopez661@gmail.com>
2025-10-01 21:41:34 -0300

    DTOs
    DTOs  -> Indo para repositories

---
+++ ecoville-back/src/main/java/senai/lab365/futurodev/ecoville_back/DTO/UsuarioDTO.java
@@ -0,0 +1,9 @@
+package senai.lab365.futurodev.ecoville_back.DTO;
+
+public class UsuarioDTO {
+    private Long id;
+    private String nome;
+    private String email;
+    private Perfil perfil;
+    private EnderecoDTO endereco;
+}
