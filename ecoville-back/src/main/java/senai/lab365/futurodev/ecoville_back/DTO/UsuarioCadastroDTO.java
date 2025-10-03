deb6b0a55a642e4684868dbb59b3bedd2a41d4d9
Renan Nandi <renannandi@gmail.com>
2025-10-03 19:51:49 -0300

    Remove arquivos duplicados

--- ecoville-back/src/main/java/senai/lab365/futurodev/ecoville_back/DTO/UsuarioCadastroDTO.java
+++
@@ -1,8 +0,0 @@
-package senai.lab365.futurodev.ecoville_back.DTO;
-
-public class UsuarioCadastroDTO {
-    private String nome;
-    private String email;
-    private String senha;
-    private EnderecoDTO endereco;
-}
