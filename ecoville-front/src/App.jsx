65adc1f6757244de814bd8be15a2756feef71e09
Marcos Vinicius Santos da Silva <marcos_viny_12@outlook.com>
2025-10-09 23:03:15 -0300

    Revert "Feat: Tela de solicitações (coletor)"
    This reverts commit a6dc9f33428af08aa2948958ec0ef932e3fcbe93.

--- ecoville-front/src/App.jsx
+++ ecoville-front/src/App.jsx
@@ -4,7 +4,6 @@ import Login from "./pages/Login/Login";
 import Register from "./pages/Register/Register";
 import Home from "./pages/Home/Home";
 import Solicitacoes from "./pages/Solicitacoes/Solicitacoes"
-import Coletas from "./pages/Coletas/Coletas"
 import Places from "./pages/Places/Places";
 import RegisterPlace from "./pages/RegisterPlace/RegisterPlace";
 import { ToastContainer } from "react-toastify";
@@ -21,7 +20,6 @@ function App() {
         <Route path="/criar-conta" Component={Register} />
         <Route path="/home" Component={Home} />
         <Route path="/solicitacoes" Component={Solicitacoes} />
-        <Route path="/coletas" Component={Coletas} />
         <Route path="/locais" Component={Places} />
         <Route path="/locais/novo" Component={RegisterPlace} />
       </Routes>
